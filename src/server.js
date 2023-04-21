70135fa3abe5591fec3e6383f3b04444be0cbc3f
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2023-04-21 16:59:08 -0300

    Aula 05 - Configuração de CORS (Cross-origin Resource Sharing)

--- src/server.js
+++ src/server.js
@@ -7,6 +7,15 @@ import sequelize from './config/database-connection.js';
 
 const app = express();
 
+// Cabeçalhos adicionados antes que as rotas sejam definidas
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler); // Manipulador de erro global (error handler)

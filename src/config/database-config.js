b6599e52da966a644894f733dd52608bae01d468
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2025-05-29 21:15:17 -0300

    Update database-config.js

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -32,10 +32,10 @@ export const databaseConfig = {
 // Configuração do banco de dados no ambiente de produção
 export const databaseConfig = {
   dialect: 'postgres',
-  host: 'dpg-cufltkjtq21c73f7ado0-a.oregon-postgres.render.com',
+  host: 'dpg-d0s9pibipnbc73eq091g-a.oregon-postgres.render.com',
   username: 'scv_backend_node_sequelize_user',
-  password: 'LYYIg8R0W4A4fUpQl7r6oTa8Ko7JiLBL',
-  database: 'scv_backend_node_sequelize_ulw1',
+  password: 'jMkjyBFi4PvIsufvNUPO1LyddGTdYqE5',
+  database: 'scv_backend_node_sequelize_lh1v',
   define: {
     timestamps: true,
     freezeTableName: true,

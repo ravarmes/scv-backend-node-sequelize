6591691890c6e7c9e8c7e948ae5d3c2a3f518afe
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2026-06-18 09:32:15 -0300

    fix: update production database connection details

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -32,10 +32,10 @@ export const databaseConfig = {
 // Configuração do banco de dados no ambiente de produção
 export const databaseConfig = {
   dialect: 'postgres',
-  host: 'dpg-d8pt4akm0tmc73b7fk50-a.oregon-postgres.render.com',
+  host: 'dpg-d8pu88k8aovs739uvk3g-a.oregon-postgres.render.com',
   username: 'scv_backend_node_sequelize_user',
-  password: 'PgMwSRDluyEAp3moRrGX5RhC0GZoj02a',
-  database: 'scv_backend_node_sequelize_db_lc1s',
+  password: 'rIXGlSbO2pCaMMwiEDATB0pH8qdTekS6',
+  database: 'scv_backend_node_sequelize_db_9ns3',
   define: {
     timestamps: true,
     freezeTableName: true,
